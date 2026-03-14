e787a9b03988e2d5fad9d0222e3d160aff36c0b1
Istratius-pixel <Istratius@mail.ru>
2026-03-15 01:38:57 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,12 @@
 'use client';
+import dynamic from 'next/dynamic';
+
+// Мы загружаем основной компонент динамически только в браузере
+const EchoApp = dynamic(() => import('./echo-content'), { 
+  ssr: false,
+  loading: () => <div style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh', background:'#000', color:'#fff'}}>Loading ECHO...</div>
+});
+
+export default function Page() {
+  return <EchoApp />;
+}
